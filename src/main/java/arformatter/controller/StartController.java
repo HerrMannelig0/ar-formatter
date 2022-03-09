f406cfb0fb53d23f7d3cfd34da287ef569b4c6d7
Michal_Rozycki <mrozycki.contractor@libertyglobal.com>
2022-03-09 09:56:15 +0100

    Project start

--- src/main/java/arformatter/controller/StartController.java
+++ src/main/java/arformatter/controller/StartController.java
@@ -14,7 +14,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
-@Controller("format")
+@Controller
 public class StartController {
 
     private final WordReader wordReader;
